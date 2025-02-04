2cccc8a5e5a6798a4fe3eebedaec61c6fe164178
dylan-source <122964402+dylan-source@users.noreply.github.com>
2025-02-04 18:21:45 +0200

    Env template

---
+++ env_template.py
@@ -0,0 +1,16 @@
+# RPC connections
+WS_URL = ""
+RPC_URL = ""
+METIS_RPC_URL = ""
+# RPC_URL = ""
+
+# Trading wallet details
+WALLET_ADDRESS = ""
+COLD_WALLET_ADDRESS = ""
+PRIVATE_KEY = ""
+
+# Rugcheck API signature
+RUGCHECK_SIGNATURE = ""
+
+# TweetScout API key
+TWEET_SCOUT_API_KEY = ""
