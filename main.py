34562eb2f2699616d405812387b72a74bed62ccf
dylan-source <122964402+dylan-source@users.noreply.github.com>
2025-02-08 19:45:52 +0200

    08_feb_v1

--- main.py
+++ main.py
@@ -35,14 +35,16 @@ async def consume_queue(queue, httpx_client):
         if token_address is None: 
             migrations_logger.info(f"Consumer triggered with no token")
 
+        print(token_address)
+
         # Run the various filters and save the info for future analysis
-        filters_result, data_to_save = await process_new_tokens(httpx_client=httpx_client, token_address=token_address, pair_address=pair_address)
+        # filters_result, data_to_save = await process_new_tokens(httpx_client=httpx_client, token_address=token_address, pair_address=pair_address)
         
-        pprint(filters_result)
-        pprint(data_to_save)
+        # pprint(filters_result)
+        # pprint(data_to_save)
 
         # Save results to a CSV for further analysis
-        await parse_migrations_to_save(token_address=token_address, pair_address=pair_address, data_to_save=data_to_save, filters_result=filters_result)
+        # await parse_migrations_to_save(token_address=token_address, pair_address=pair_address, data_to_save=data_to_save, filters_result=filters_result)
 
 async def main():
     
